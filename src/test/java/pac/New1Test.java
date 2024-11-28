c0089f3e44ca8a96f153f0b7b2c4479977847a6e
pragti gupta <pragtig6@gmail.com>
2024-11-28 16:37:14 +0530

    dscccs

--- src/test/java/pac/New1Test.java
+++ src/test/java/pac/New1Test.java
@@ -3,6 +3,14 @@ package pac;
 import org.testng.annotations.Test;
 
 public class New1Test {
+	
+	// Pipeline Script using SCM (Git):
+	
+	//https://www.youtube.com/watch?v=hIdwlo3pl3k	
+	//https://github.com/kamalgirdher/jenkins-tutorials/tree/main
+		
+	
+	
   @Test
   public void firTest() {
 	  
--- src/test/java/pac/New2Test.java
+++ src/test/java/pac/New2Test.java
@@ -16,4 +16,8 @@ public class New2Test {
   }
   
   
+  
+  
+  
+  
 }
